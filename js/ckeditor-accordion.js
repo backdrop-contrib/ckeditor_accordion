d076392f8b506724f27337281246d70f3b149bba
Herb v/d Dool <herb@3speedhub.com>
2026-08-02 19:16:56 -0400

    Add new settings from Drupal

--- js/ckeditor-accordion.js
+++ js/ckeditor-accordion.js
@@ -7,9 +7,10 @@
   'use strict';
   Backdrop.behaviors.ckeditorAccordion = {
     attach: function (context, settings) {
+      var accordionSettings = settings.ckeditor_accordion || {};
 
       // Create accordion functionality if the required elements exist is available.
-      var $ckeditorAccordion = $('.ckeditor-accordion');
+      var $ckeditorAccordion = $('.ckeditor-accordion', context);
       if ($ckeditorAccordion.length > 0) {
         // Create simple accordion mechanism for each tab.
         $ckeditorAccordion.each(function () {
@@ -18,24 +19,38 @@
             return;
           }
 
+          var animate = accordionSettings.animateAccordionOpenAndClose;
+          var collapseAll = accordionSettings.collapseAll;
+          var allowHtmlInTitles = accordionSettings.allowHtmlInTitles;
+          var openTabsWithHash = accordionSettings.openTabsWithHash;
+
           // The first one is the correct one.
-          if (typeof Backdrop.settings.ckeditor_accordion == 'undefined' ||
-             (typeof Backdrop.settings.ckeditor_accordion != 'undefined' && Backdrop.settings.ckeditor_accordion.collapseAll != 1)) {
-            $accordion.children('dt:first').addClass('active');
-            $accordion.children('dd:first').addClass('active');
-            $accordion.children('dd:first').css('display', 'block');
+          if (!collapseAll) {
+            $accordion.children('dt:first-child').addClass('active');
+            $accordion.children('dd:first-of-type').addClass('active').css('display', 'block');
           }
 
           // Turn the accordion tabs to links so that the content is accessible & can be traversed using keyboard.
           $accordion.children('dt').each(function () {
             var $tab = $(this);
             var tabText = $tab.text().trim();
+            var tabHtml = (allowHtmlInTitles) ? $tab.html().trim() : tabText;
             var toggleClass = $tab.hasClass('active') ? ' active' : '';
-            $tab.html('<span class="ckeditor-accordion-toggle' + toggleClass + '"></span><a class="ckeditor-accordion-toggler" href="#">' + tabText + '</a>');
+            var hrefAndIds = 'href="#"';
+            if (openTabsWithHash) {
+              var tabHash = encodeURIComponent(tabText.replace(/[^A-Za-z0-9]/g, ''));
+              hrefAndIds = 'href="#' + tabHash + '" id="' + tabHash + '" onclick="return false;"';
+            }
+            $tab.html('<a class="ckeditor-accordion-toggler" ' + hrefAndIds + '><span class="ckeditor-accordion-toggle' + toggleClass + '"></span>' + tabHtml + '</a>');
           });
 
           // Wrap the accordion in a div element so that quick edit function shows the source correctly.
-          $accordion.addClass('styled').removeClass('ckeditor-accordion').wrap('<div class="ckeditor-accordion-container"></div>');
+          var wrapClasses = 'ckeditor-accordion-container';
+          if (!animate) {
+            wrapClasses = wrapClasses + ' no-animations';
+          }
+          $accordion.addClass('styled').removeClass('ckeditor-accordion');
+          $accordion.wrap('<div class="' + wrapClasses + '"></div>');
 
           // Fire an event so other modules know when the accordion has been created.
           $accordion.trigger('ckeditorAccordionAttached');
@@ -45,27 +60,79 @@
         $('body').once('ckeditorAccordionToggleEvent').on('click', '.ckeditor-accordion-toggler', function (e) {
           var $t = $(this).parent();
           var $parent = $t.parent();
+          var keepRowsOpen = accordionSettings.keepRowsOpen;
+          var animate = accordionSettings.animateAccordionOpenAndClose;
 
           // Clicking on open element, close it.
           if ($t.hasClass('active')) {
             $t.removeClass('active');
-            $t.next().slideUp();
+            $t.children('a').children('span').removeClass('active');
+            if (animate) {
+              $t.next().slideUp(300, function () { $(this).removeClass('active'); });
+            }
+            else {
+              $t.next().hide().removeClass('active');
+            }
           }
           else {
-            // Remove active classes.
-            $parent.children('dt.active').removeClass('active').children('a').removeClass('active');
-            $parent.children('dd.active').slideUp(function () {
-              $(this).removeClass('active');
-            });
+            if (!keepRowsOpen) {
+              // Remove active classes.
+              $parent.children('dt.active').removeClass('active').children('a').children('span').removeClass('active');
+              $parent.children('dd.active').each(function () {
+                var $dd = $(this);
+                if (animate) {
+                  $dd.slideUp(300, function () { $dd.removeClass('active'); });
+                }
+                else {
+                  $dd.hide().removeClass('active');
+                }
+              });
+            }
 
             // Show the selected tab.
             $t.addClass('active');
-            $t.next().slideDown(300).addClass('active');
+            $t.children('a').children('span').addClass('active');
+            if (animate) {
+              $t.next().slideDown(300).addClass('active');
+            }
+            else {
+              $t.next().show().addClass('active');
+            }
           }
 
           // Don't add hash to url.
           e.preventDefault();
         });
+
+        // Open tabs with hash if config requires.
+        if (accordionSettings.openTabsWithHash) {
+          // Trigger hash change when clicking an anchor to an accordion tab on the same page.
+
+          // Open content that matches the hash on hash change.
+          var handleHash = function () {
+            var hash = window.location.hash;
+            if (!hash) {
+              return;
+            }
+            var $toggler = $('a.ckeditor-accordion-toggler[href="' + hash + '"]');
+            if ($toggler.length && !$toggler.parent().hasClass('active')) {
+              $toggler.click();
+            }
+          };
+
+          // Handle links pointing to accordion hashes.
+          $('a[href*="#"]').once('ckeditorAccordionHashLinks').on('click', function (e) {
+            var href = $(this).attr('href');
+            var hash = href.substring(href.indexOf('#'));
+            if ($('a.ckeditor-accordion-toggler[href="' + hash + '"]').length) {
+              window.location.hash = hash;
+              e.preventDefault();
+            }
+          });
+
+          $(window).on('hashchange', handleHash);
+          handleHash();
+        }
       }
     }
   };
